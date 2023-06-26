9afcedc271371e58e351fbfa43602f76170f963e
Nadia Bugarin <t-nbugarin@microsoft.com>
2023-06-26 14:00:43 -0700

    Update for key, id, and value for Asynchronization for Redis Cache and CosmosDB listrigger

--- samples/dotnet/ListTrigger.cs
+++ samples/dotnet/ListTrigger.cs
@@ -6,15 +6,15 @@ using System.Collections;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
 {
-    public static class RedisSamples
+    public static class ListTrigger
     {
-        public const string localhostSetting = "Redis Cache Primary Connection String";
+        public const string localhostSetting = "redisLocalhost";
 
         //connecting to CosmosDB
-        //primary connection string from your database in "Account Endpoint here"
-        static readonly string Endpoint = "CosmosDB Account Endpoint here";
+        //primary connection string
+        static readonly string Endpoint = "AccountEndpoint=https://cosbackup.documents.azure.com:443/;AccountKey=yoctt2Rr0r3Y9hUPPIBO99NE36J877WDWgThLuI7zJdnO2rdNnKB0v5UuCQLAGxfdpBGPvaFafdCACDboQdAAw==;";
         static readonly CosmosClient cc = new CosmosClient(Endpoint);
-        static readonly Cosmos.Container db = cc.GetDatabase("Container Name").GetContainer("Container ID Name");
+        static readonly Container db = cc.GetDatabase("backup").GetContainer("backcos");
 
         [FunctionName(nameof(ListTriggerAsync))]
         public static async Task ListTriggerAsync(
@@ -28,52 +28,11 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
             Hashtable pair = new Hashtable()
             {
                 { "id", id },  // Unique identifier for the document
-                { "key1", entry }
+                { "key", "listTest" },
+                {"value", entry }
             };
 
-            ItemResponse<Hashtable> response = await db.CreateItemAsync(pair);
-
-            Hashtable newpair = response.Resource;
-        }
-
-        [FunctionName(nameof(pubsubtrigger))]
-        public static void pubsubtrigger(
-            [RedisPubSubTrigger(localhostSetting, "pubsubtest")] string message,
-            ILogger logger)
-        {
-            logger.LogInformation(message);
-        }
-
-        [FunctionName(nameof(pubsubtriggerresolvedchannel))]
-        public static void pubsubtriggerresolvedchannel(
-            [RedisPubSubTrigger(localhostSetting, "%pubsubchannel%")] string message,
-            ILogger logger)
-        {
-            logger.LogInformation(message);
-        }
-
-        [FunctionName(nameof(keyspacetrigger))]
-        public static void keyspacetrigger(
-            [RedisPubSubTrigger(localhostSetting, "__keyspace@0__:keyspacetest")] string message,
-            ILogger logger)
-        {
-            logger.LogInformation(message);
-        }
-
-        [FunctionName(nameof(keyeventtrigger))]
-        public static void keyeventtrigger(
-            [RedisPubSubTrigger(localhostSetting, "__keyevent@0__:del")] string message,
-            ILogger logger)
-        {
-            logger.LogInformation(message);
-        }
-
-        [FunctionName(nameof(streamtrigger))]
-        public static void streamtrigger(
-            [RedisPubSubTrigger(localhostSetting, "streamtest")] string entry,
-            ILogger logger)
-        {
-            logger.LogInformation(entry);
+            await db.CreateItemAsync(pair);
         }
     }
 }
