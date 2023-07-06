f38345d774228f5bde70f3090bf3763aaba40a9f
Nadia Bugarin <t-nbugarin@microsoft.com>
2023-07-06 14:28:23 -0700

    CosmosToRedis Function for List Trigger and Synchronization ListTrigger for Redis Cache to CosmosDB

---
+++ CosmosToRedis/CosmosToRediscs.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using StackExchange.Redis;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using Container = Microsoft.Azure.Cosmos.Container;
+
+namespace CosmosToRedis
+{
+    public record ListData
+    (
+        string id,
+        List<string> value
+    );
+
+    internal class CosmosToRediscs
+    {
+        //redis connection string
+        static ConnectionMultiplexer redisconnect = ConnectionMultiplexer.Connect("RediscachePrimaryString");
+        static IDatabase cache = redisconnect.GetDatabase();
+        public const string localhostSetting = "redisLocalhost";
+
+        //connecting to CosmosDB
+        //primary connection string
+        static readonly string Endpoint = "Endpoint";
+        static readonly CosmosClient cc = new CosmosClient(Endpoint);
+        static readonly Microsoft.Azure.Cosmos.Container db = cc.GetDatabase("databasename").GetContainer("containername");
+
+        [FunctionName("CosmosToRedis")]
+        public static void Run([CosmosDBTrigger(
+        databaseName: "databasename",
+        containerName: "continername",
+        Connection = "connectionstringdb",
+        LeaseContainerName = "leases")]IReadOnlyList<ListData> input, ILogger log)
+        {
+            if (input == null || input.Count <= 0) return;
+
+            var cache = redisconnect.GetDatabase();
+
+            Console.WriteLine(input);
+
+            foreach (var value2 in input)
+            {
+                RedisValue[] redisValues = Array.ConvertAll(value2.value.ToArray(), item => (RedisValue)item);
+                foreach (var value in redisValues)
+                {
+                    cache.ListRightPush("listName", value);
+                    log.LogInformation($"Saved item with id {input.Count} in Azure Redis cache");
+                }
+            }
+        }
+    }
+}
---
+++ WriteThrough-/ListTriggerSync.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using System.Collections;
+using System.ComponentModel;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Redis;
+using System.Collections.Generic;
+using StackExchange.Redis;
+using System.Linq;
+using Microsoft.Azure.Cosmos.Linq;
+
+namespace WriteThrough
+{
+    public record ListData
+    (
+        string id,
+        List<string> value
+    );
+    internal class WriteThrough
+    {
+        //redis connection string
+        static ConnectionMultiplexer redisconnect = ConnectionMultiplexer.Connect("RediscachePrimaryString");
+        static IDatabase cache = redisconnect.GetDatabase();
+        public const string localhostSetting = "redisLocalhost";
+
+        //connecting to CosmosDB
+        //primary connection string
+        static readonly string Endpoint = "Endpoint";
+        static readonly CosmosClient cc = new CosmosClient(Endpoint);
+        static readonly Microsoft.Azure.Cosmos.Container db = cc.GetDatabase("databasename").GetContainer("containername");
+
+        [FunctionName(nameof(ListTrigger))]
+        public static void ListTrigger(
+            [RedisListTrigger(localhostSetting, "listName")] string entry,
+            ILogger logger)
+        {
+            var query = db.GetItemLinqQueryable<ListData>();
+            using FeedIterator<ListData> f = query
+                .Where(p => p.id == "listName")
+                .ToFeedIterator();
+            //if there doesnt exist an entry with this key in cosmos
+
+            //.Result blocks the execution until the result is available
+            //this can cause deadlock so thats why asynch is preferred
+            var response = f.ReadNextAsync().Result;
+            var item = response.FirstOrDefault(defaultValue: null);
+            if (item == null)
+            {
+                Console.WriteLine("Query null");
+                logger.LogInformation(entry);
+                string value = entry.ToString();
+                //Guid id = Guid.NewGuid();
+                List<string> temp = new List<string>
+                {
+                    entry
+                };
+
+                ListData pair = new ListData(id: "listName", value: temp);
+
+                db.UpsertItemAsync(pair);
+            }
+
+            else
+            {
+                Console.WriteLine("Querry not null ");
+                logger.LogInformation(entry);
+                string value = entry.ToString();
+
+                //get the current values associated with listTest in CosmosDB
+                //ListData toDoActivity = await db.ReadItemAsync<ListData>(item.id, new PartitionKey("/id"));
+
+                List<string> temp2 = item.value;
+                Console.WriteLine("Current vals: " + item.value);
+
+                temp2.Add(entry);
+                ListData pair = new ListData(id: "listName", value: temp2);
+                List<PatchOperation> patchOperations = new List<PatchOperation>()
+                {
+                    PatchOperation.Replace("/value", temp2),
+                };
+                Console.WriteLine("Before last line ");
+
+                ItemResponse<ListData> item2 =  db.UpsertItemAsync<ListData>(pair).Result;
+            }
+        }
+
+    }
+}
