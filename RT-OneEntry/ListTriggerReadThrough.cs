04b983c05d8c333fcd4bfc0ba74b3c0eadf9d792
Nadia Bugarin <t-nbugarin@microsoft.com>
2023-07-10 14:21:02 -0700

    Debug ReadThrough for ListTrigger

--- RT-OneEntry/ListTriggerReadThrough.cs
+++ RT-OneEntry/ListTriggerReadThrough.cs
@@ -20,6 +20,8 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace RT_OneEntry
 {
+
+
     public record ListData
     (
         string id,
@@ -34,44 +36,35 @@ namespace RT_OneEntry
         public static async Task ListTriggerReadThroughFunc(
             [RedisPubSubTrigger(localhostSetting, "__keyevent@0__:keymiss")] string listEntry, [CosmosDB(
                             databaseName: "dbname",
-                            containerName: "containername",
-                            Connection = "endpoint" )]CosmosClient input,
+                            containerName: "contname",
+                            Connection = "Endpoint" )]CosmosClient input,
             ILogger logger)
         {
-            Container db = input.GetDatabase("back").GetContainer("async");
-            if(await cache.KeyExistsAsync(listEntry) == true){
-                Console.WriteLine("This entry exists in the the cache.");
+            Container db = input.GetDatabase("dbname").GetContainer("contname");
+            var query = db.GetItemLinqQueryable<ListData>();
+            using FeedIterator<ListData> results = query
+                .Where(p => p.id == "listTest")
+                .ToFeedIterator();
+            var response = await results.ReadNextAsync();
+            var item = response.FirstOrDefault(defaultValue: null);
+
+            //if there doesnt exist an entry with this key in cosmos, 
+            if (item == null){
+                return;
             }
 
-            //else, go to cosmos and bring to redis
+            //else, bring from cosmos to redis
             else{
-                Console.WriteLine("This entry does not exist in the the cache.");
-                var query = db.GetItemLinqQueryable<ListData>();
-                using FeedIterator<ListData> results = query
-                    .Where(p => p.id == "listTest")
-                    .ToFeedIterator();
-                var response = await results.ReadNextAsync();
-                var item = response.FirstOrDefault(defaultValue: null);
-
-                //if there doesnt exist an entry with this key in cosmos, 
-                if (item == null){
-                    Console.WriteLine("This key does not exist in CosmosDB either.");
-                }
-
-                //else, bring from cosmos to redis
-                else{
-                    Console.WriteLine("The key is in CosmosDB ");
-                    logger.LogInformation(listEntry);
-                    //get the amount of elements in cosmos associated with the key, so you can access each item
-                    var item2 = response.Take(response.Count);
-                    if (item2 == null) return;
-                    foreach (var val in item2){
-                        RedisValue[] redisValues = Array.ConvertAll(val.value.ToArray(), item => (RedisValue)item);
-                        foreach (var value2 in redisValues)
-                        {
-                            await cache.ListRightPushAsync("listTest", value2);
-                            Console.WriteLine($"Saved item");
-                        }
+                logger.LogInformation(listEntry);
+                //get the amount of elements in cosmos associated with the key, so you can access each item
+                var item2 = response.Take(response.Count);
+                if (item2 == null) return;
+                foreach (var val in item2)
+                {
+                    RedisValue[] redisValues = Array.ConvertAll(val.value.ToArray(), item => (RedisValue)item);
+                    foreach (var value2 in redisValues)
+                    {
+                        await cache.ListRightPushAsync("listTest", value2);
                     }
                 }
             }
